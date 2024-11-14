957b32a05161de647ee4dd35770e2b21bcb22f57
Tatyana <juravleva.tanya01@yandex.ru>
2024-11-14 17:53:34 +0300

    connect to PostgreSQL and base django migrations

--- library/library/settings.py
+++ library/library/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'LibraryAdmin',
+        'PASSWORD': 'LibraryAdmin2024',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
