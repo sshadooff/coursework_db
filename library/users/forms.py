da7cf95ec293ec4da673a3fa0f42eff517f935d2
Tatyana <juravleva.tanya01@yandex.ru>
2024-11-20 15:17:56 +0300

    user registration has been done and the appearance has been configured for staff and regular users

--- library/users/forms.py
+++ library/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -7,3 +7,31 @@ from users.models import User
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
+        fields = ["username", "password"]
+
+    username = forms.CharField()
+    password = forms.CharField()
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "email",
+            "phone",
+            "password1",
+            "password2",
+        ]
+
+    username = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    date_of_birth = forms.CharField()
+    email = forms.CharField()
+    phone = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
